1594ddf5e5d654701a94f5d1aa1d3f3ec84fb154
sridharfinelabs <sridhar@finelabs.co>
2024-07-17 14:18:36 +0530

    Update main.py
    code updated to create destination TF files

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import os
 import openai
 from dotenv import load_dotenv
-import json
 
 
-def convert_tf_to_arm(input_dir, output_dir):
+def convert_aws_tf_to_azm_tf(input_dir, output_dir):
     # Traverse the directory tree
     for root, dirs, files in os.walk(input_dir):
         for file in files:
@@ -14,57 +13,45 @@ def convert_tf_to_arm(input_dir, output_dir):
                 target_dir = os.path.join(output_dir, relative_path.replace("aws", "azm"))
                 os.makedirs(target_dir, exist_ok=True)
                 # Change the file extension from .tf to .json
-                target_file = os.path.splitext(file)[0] + ".json"
+                # target_file = os.path.splitext(file)[0] + ".tf"
                 # Construct the full path to the target file
-                target_path = os.path.join(target_dir, target_file)
+                target_path = os.path.join(target_dir, file)
                 # Read the content of the source file
-                with open(source_path, 'r', encoding='utf-8') as f:
+                with open(source_path, 'r') as f:
                     file_content = f.read()
                 # Get the conversion result
-                result = create_chat_response(file_content).replace("\n","").rstrip()
-                # out = clean_json(result)
-                print(is_json(result))
-                # print(result)
-                json_object = json.loads(result)
-                # Write the result to the target file in JSON format
-                with open(target_path, 'w', encoding='utf-8') as f:
-                    json.dump(json_object, f, indent=4, sort_keys=True)
-
+                result = convert_aws_to_azure_terraform(file_content)
+                with open(target_path, 'w') as tf:
+                        tf.write(result)
                 print(f"Converted {source_path} to {target_path}")
                 print("Converted...")
                 
                 
-def is_json(myjson):
-    try:
-        json.loads(myjson)
-    except ValueError as e:
-        return False
-    return True
-
-
-# Function to convert Terraform to Azure ARM
-def create_chat_response(message):
-    messages = [
-        {"role": "system", "content": "Convert the following Terraform code to an Azure Resource Manager (ARM) template and \
-         give the completed json format dont inlcude apiVersion this is gpt generated code of arm file could you explain me remove this from gpt response need code to \
-         remove this line dont give complete summary code show remove response code "},
-        {"role": "user", "content": message},
-        {"role": "assistant", "content": message}
-    ]
-    
+def convert_aws_to_azure_terraform(aws_terraform_code):
+    # Create a prompt for ChatGPT
+#     prompt = f"Convert the following AWS Terraform IaC to an Azure Terraform IaC template, Provide the complete Azure Terraform template."
+    messages=[
+        {"role": "system", "content": "Convert the following AWS Terraform IaC to an Azure Terraform IaC template, Provide the complete Azure Terraform template "},
+            {"role": "system", "content": "You are a helpful assistant that converts AWS Terraform IaC to Azure Terraform IaC."},
+            {"role": "user", "content": aws_terraform_code}
+        ]
+    # Use OpenAI's Chat Completion
     response = openai.chat.completions.create(
-            model="gpt-3.5-turbo-16k",
-            messages=messages,
-            temperature=0.5,
-            max_tokens=1200,
-            top_p=1,
-            n=1,
-            stop=None,
-            frequency_penalty=0,
-            presence_penalty=0
-        )
-    
-    return response.choices[0].message.content
+        model="gpt-3.5-turbo-16k",
+        messages=messages,
+        temperature=0.5,
+        max_tokens=1200,
+        top_p=1,
+        n=1,
+        stop=None,
+        frequency_penalty=0,
+        presence_penalty=0
+
+    )
+
+    azure_terraform_code = response.choices[0].message.content
+    return azure_terraform_code
+
 
 def main():
     load_dotenv()
@@ -73,10 +60,9 @@ def main():
     input_dir = "terraform_file"#input("Enter or paste Input directory: ")
     output_dir = "output"#input("Enter or paste output directory: ")
     print("Processing......")
-    convert_tf_to_arm(input_dir, output_dir)
+    convert_aws_tf_to_azm_tf(input_dir, output_dir)
     print("Processing Completed... ")
 
 if __name__ == "__main__":
     main()
     
-
